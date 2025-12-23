6ab7ef736ac613ebfc21384ecf8cd3bb77db4fa2
benny_neiman <bn161299@gmail.com>
2025-12-23 11:50:54 +0200

    routes

--- app.js
+++ app.js
@@ -3,3 +3,17 @@ import express from 'express'
 const app = express()
 
 const port = 3002;
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`server runing on http://localhost:${port}`);
+});
